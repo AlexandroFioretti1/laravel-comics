e017975ebc0b875ba39581da88083442ac75d842
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-05-23 16:42:32 +0200

    add img on footer

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,19 @@
-<div class="container text-center">
-    <h1>funziono bene</h1>
+<div class="container h-100">
+    <div class="d-flex justify-content-between align-items-center h-100">
+        <div>
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="" />
+        </div>
+        <nav class="h-100 d-flex">
+            <a class="d-flex align-items-center h-100" href="#">CHARACTERS</a>
+            <a class="d-flex align-items-center h-100 c_blue" href="#">COMICS</a>
+            <a class="d-flex align-items-center h-100" href="#">MOVIES</a>
+            <a class="d-flex align-items-center h-100" href="#">TV</a>
+            <a class="d-flex align-items-center h-100" href="#">GAMES</a>
+            <a class="d-flex align-items-center h-100" href="#">COLLECTIBLES</a>
+            <a class="d-flex align-items-center h-100" href="#">VIDEOS</a>
+            <a class="d-flex align-items-center h-100" href="#">FANS</a>
+            <a class="d-flex align-items-center h-100" href="#">NEWS</a>
+            <a class="d-flex align-items-center h-100" href="#">SHOP</a>
+        </nav>
+    </div>
 </div>
